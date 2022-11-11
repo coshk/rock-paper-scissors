00a5e7a6775c9a6b94bca81ba20f7c453103de9a
Ivan Kotov <iamivankotov@gmail.com>
2022-11-11 17:27:33 +0000

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Choice from './components/Choice/Choice';
+import Game from './components/Game/Game';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          rock paper scissors game
+        </h1>
       </header>
+      <main>
+        <Choice />
+        <Game />
+      </main>
     </div>
   );
 }
